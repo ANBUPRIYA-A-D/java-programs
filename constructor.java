8776daea3616495665c5753127c94f0390e74329
ANBUPRIYA-A-D <103745618+ANBUPRIYA-A-D@users.noreply.github.com>
2022-04-15 17:16:06 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,11 @@
+class AdditionData{
+   AdditionData(){
+   System.out.println("Constructor");
+}
+}
+public class Constructor {
+   public static void main(String[] args) {
+      new AdditionData();
+      
+   }
+}
