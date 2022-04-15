51204d6194c93581e25d16c262a527e39e28aa6f
ANBUPRIYA-A-D <103745618+ANBUPRIYA-A-D@users.noreply.github.com>
2022-04-15 17:32:24 +0530

    Create Ooverload.java

---
+++ Ooverload.java
@@ -0,0 +1,13 @@
+public class OOverload{
+     int a=6,b=7;
+     public static void main(String args[]){
+   
+    OOverload obj = new OOverload();
+    System.out.println("a+b="+obj.a+obj.b);
+    
+     System.out.println("a+b="+(obj.a+obj.b));
+        
+    
+    
+     }
+}
